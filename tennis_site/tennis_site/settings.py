3be169cebca9db0b2bf67fd8f97fe5b9a29775e7
Ибдиминов Тимур <tipbandit52@gmail.com>
2025-10-05 21:14:48 +0500

    фикс настроек домена и базы

--- tennis_site/tennis_site/settings.py
+++ tennis_site/tennis_site/settings.py
@@ -21,8 +21,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "https://tennis-site.onrender.com",
-    "litetennis.kz",
-    "www.litetennis.kz",
+    "https://litetennis.kz",
+    "https://www.litetennis.kz",
 ]
 SESSION_COOKIE_SAMESITE = None
 SESSION_COOKIE_SECURE = not DEBUG
